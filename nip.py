c4f761b84d980183a77ba37c729a415342505bd5
nkjurczyk <nkjurczyk@gmail.com>
2016-11-17 21:41:49 +0100

    Poprawa uwag z 2. maila

--- nip.py
+++ nip.py
@@ -8,7 +8,7 @@ scale = [6,5,7,2,3,4,5,6,7]
 while True:
     nip=[]
     i=0
-    last1=0
+    control_sum=0
     for i in range(0,9):
         if i == 0 or i == 2:
             number = random.randint(1, 9)
@@ -16,16 +16,15 @@ while True:
             number = random.randint(0, 9)
 
         nip.append(number)
-        last1 = last1 + (nip[i] * scale[i])
-        i = len(nip)
+        control_sum = control_sum + (nip[i] * scale[i])
 
-    last = last1 % 11
-    print last
 
-    if last == 10:
+    control_number = control_sum % 11
+
+    if control_number == 10:
         continue
     else:
-        nip.append(last)
+        nip.append(control_number)
         print ''.join(str(i) for i in nip)
 
     print 'Wygenerować kolejny nip (t/n)?'
